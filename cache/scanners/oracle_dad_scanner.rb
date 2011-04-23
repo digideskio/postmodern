6d94818a8b60a8608ce7b9fcd228a69b3927c6c8
Postmodern <postmodern.mod3@gmail.com>
2011-04-23 10:47:53 -0700

    Ported the Oracle DAD Scanner from carnal0wnage as a URLScanner.

---
+++ cache/scanners/oracle_dad_scanner.rb
@@ -0,0 +1,48 @@
+require 'ronin/scanners/url_scanner'
+
+Ronin::Scanners::URLScanner.object do
+
+  parameter :host, :description => 'The host to check'
+  parameter :port, :default => 80,
+                   :description => 'The port to check'
+
+  cache do
+    self.name = 'Oracle Application Server Detection'
+    self.version = '0.1'
+    self.description = %{
+      This scans for common ORACLE Database Access Desriptors (DAD)
+    }
+
+    self.author(:name => 'CG', :organization => 'carnal0wnage')
+    self.license!(:mit)
+  end
+
+  def scan
+    Net.http_session(:host => @host, :port => @port) do |http|
+      paths = YAML.load_file(find_data_file('oracle/dad_fingerprints.yml'))
+
+      paths.each do |path|
+        response = begin
+                     http.get(path)
+                   rescue Exception
+                     print_error "HTTP Request failed for #{path}"
+                     next
+                   end
+
+        case response.code
+        when '200'
+          print_info "HTTP 200 for DAD: #{path}"
+
+          yield "http://#{@host}:#{@port}#{path}"
+        when '302', '301'
+          location = response.headers['Location']
+
+          print_info "HTTP #{response.code} for DAD: #{path} -> #{location}"
+        else
+          print_debug "HTTP #{response.code} for #{path}"
+        end
+      end
+    end
+  end
+
+end
