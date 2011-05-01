d2eee8237e4762def2c9c35586cf79ffceb4fa13
Postmodern <postmodern.mod3@gmail.com>
2011-04-30 21:30:50 -0700

    Added a dork for finding those leaked PGP Private Keys.

---
+++ cache/dorks/pgp_private_keys.rb
@@ -0,0 +1,19 @@
+require 'ronin/dorks/google_dork'
+
+Ronin::Dorks::GoogleDork.object do
+
+  cache do
+    self.name = 'PGP Private Keys'
+    self.description = %{
+      Finds leaked PGP Private Keys.
+    }
+
+    author :name => 'Postmodern', :organization => 'SophSec'
+    license! :mit
+  end
+
+  dork do
+    query(:intext => 'BEGIN PGP PRIVATE KEY BLOCK', :filetype => 'asc')
+  end
+
+end
